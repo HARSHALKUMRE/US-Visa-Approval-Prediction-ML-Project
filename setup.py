4c27384a2876cccd38c9c615329ea357657f7c9e
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-02-28 11:26:13 +0530

    project setup file and requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Harshal Kumre",
+    author_email="kumreharshalkumar@gmail.com",
+    packages=find_packages()
+)
